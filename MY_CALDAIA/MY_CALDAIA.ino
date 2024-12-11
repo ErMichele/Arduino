1aa087b94646fdeb501c005a4f6da6498361394f
Michele Acciuffi <micheleacciuffi@gmail.com>
2024-12-11 16:15:48 +0100

    Godo

--- MY_CALDAIA/MY_CALDAIA.ino
+++ MY_CALDAIA/MY_CALDAIA.ino
@@ -1,30 +1,46 @@
 #include <LiquidCrystal.h>
+#include <DHT.h>
 
-int umi;
-int temp=10;
+int umi, temp;
+#define Sens 13
 #define on 6
 #define off 8
+
+#define DHTTYPE DHT11
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+DHT dht (Sens, DHTTYPE);
 
 void setup()
 {
+  Serial.begin(9600);
   lcd.begin(16, 2);
   lcd.print("Umidita':");
   lcd.setCursor(0, 1);
   lcd.print("Temperatura:");
   pinMode (on, OUTPUT);
   pinMode (off, OUTPUT);
+  dht.begin();
 }
-//nota bene: la temperatura non sarà usata dato che non abbiamo la componente qui, ma il funzionamento è lo stesso
+
 void loop()
 {
-  umi=analogRead(A0);
+  umi=dht.readHumidity();
+  temp=dht.readTemperature();
+  Serial.print(temp);
   if (umi<10) {
     lcd.setCursor(10, 0);
     lcd.print ("  ");
   }
   else if (umi<100) {
     lcd.setCursor(10, 0);
+    lcd.print (" ");
+  }
+  if (temp<10) {
+    lcd.setCursor(10, 1);
+    lcd.print ("  ");
+  }
+  else if (temp<100) {
+    lcd.setCursor(10, 1);
     lcd.print (" ");
   }
   lcd.setCursor(9, 0);
@@ -35,12 +51,12 @@ void loop()
   lcd.print(temp);
   lcd.setCursor (15, 1);
   lcd.print("C");
-  if (umi<438/*||temp<16*/) {
+  if (temp<25) {
     digitalWrite(on, HIGH);
     digitalWrite(off, LOW);
   }
   else {
-	digitalWrite(off, HIGH);
+	  digitalWrite(off, HIGH);
     digitalWrite(on, LOW);
   }
   delay(1000);
